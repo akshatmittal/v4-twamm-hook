97a09a547c450eec898e0bcbb415e290a45261a2
Akshat Mittal <itsreallyakshat@gmail.com>
2025-01-14 03:53:36 +0530

    Reapply "beforeRemoveLiquidity trigger as well"
    This reverts commit 510063ede192cb3741f4f1cf25a23135f41e5f56.

--- src/TWAMM.sol
+++ src/TWAMM.sol
@@ -78,7 +78,7 @@ contract TWAMM is BaseHook, Owned, ITWAMM {
             beforeInitialize: true,
             afterInitialize: false,
             beforeAddLiquidity: true,
-            beforeRemoveLiquidity: false,
+            beforeRemoveLiquidity: true,
             afterAddLiquidity: false,
             afterRemoveLiquidity: false,
             beforeSwap: true,
@@ -120,6 +120,17 @@ contract TWAMM is BaseHook, Owned, ITWAMM {
         executeTWAMMOrders(key);
 
         return BaseHook.beforeAddLiquidity.selector;
+    }
+
+    function beforeRemoveLiquidity(
+        address,
+        PoolKey calldata key,
+        IPoolManager.ModifyLiquidityParams calldata,
+        bytes calldata
+    ) external override onlyPoolManager returns (bytes4) {
+        executeTWAMMOrders(key);
+
+        return BaseHook.beforeRemoveLiquidity.selector;
     }
 
     function beforeSwap(address, PoolKey calldata key, IPoolManager.SwapParams calldata, bytes calldata)
--- test/TWAMM.complex.t.sol
+++ test/TWAMM.complex.t.sol
@@ -45,8 +45,10 @@ contract TWAMMComplexTest is Test, Fixtures {
         vm.label(address(token1), "Token1");
 
         address flags = address(
-            uint160(Hooks.BEFORE_INITIALIZE_FLAG | Hooks.BEFORE_SWAP_FLAG | Hooks.BEFORE_ADD_LIQUIDITY_FLAG)
-                ^ (0x4444 << 144) // Namespace the hook to avoid collisions
+            uint160(
+                Hooks.BEFORE_INITIALIZE_FLAG | Hooks.BEFORE_SWAP_FLAG | Hooks.BEFORE_ADD_LIQUIDITY_FLAG
+                    | Hooks.BEFORE_REMOVE_LIQUIDITY_FLAG
+            ) ^ (0x4444 << 144) // Namespace the hook to avoid collisions
         );
 
         vm.warp(TWAMM_INTERVAL);
